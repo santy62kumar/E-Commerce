9f933edd67a62f7feaae8749953ce81e7c3b5954
santy62kumar <s.santoshkumar6205@gmail.com>
2024-08-16 00:24:50 +0530

    add middleware store userdetails

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -5,6 +5,14 @@ const SummaryApi = {
         url : `${backendDomin}/api/signup`,
         method : "post"
     },
+    signIn : {
+        url : `${backendDomin}/api/signin`,
+        method : "post"
+    },
+    current_user : {
+        url : `${backendDomin}/api/user-details`,
+        method : "get"
+    },
 }
 
 export default SummaryApi
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Logo from './Logo'
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
+import {  useSelector } from 'react-redux';
+// import SummaryApi from '../common';
+// import { toast } from 'react-toastify'
+// import { setUserDetails } from '../store/userSlice';
+// import ROLE from '../common/role';
+// import Context from '../context';
 
 function Header() {
+  const user = useSelector(state => state?.user?.user)
   return (
-    
     <header className='h-16 shadow-md bg-white fixed w-full z-40'>
       <div className=' h-full container mx-auto flex items-center px-4 justify-between'>
             <div className=''>
@@ -17,7 +23,7 @@ function Header() {
             </div>
 
             <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
-                <input type='text' placeholder='search product here...' className='w-full outline-none' onChange={handleSearch} value={search}/>
+                <input type='text' placeholder='search product here...' className='w-full outline-none' />
                 <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
                   <GrSearch />
                 </div>
@@ -30,7 +36,7 @@ function Header() {
 
                   {
                     user?._id && (
-                      <div className='text-3xl cursor-pointer relative flex justify-center' onClick={()=>setMenuDisplay(preve => !preve)}>
+                      <div className='text-3xl cursor-pointer relative flex justify-center' >
                         {
                           user?.profilePic ? (
                             <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt={user?.name} />
@@ -42,7 +48,7 @@ function Header() {
                     )
                   }
                   
-                  
+{/*                   
                   {
                     menuDisplay && (
                       <div className='absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded' >
@@ -57,7 +63,7 @@ function Header() {
                       </div>
                     )
                   }
-                 
+                  */}
                 </div>
 
                   {
@@ -65,26 +71,26 @@ function Header() {
                       <Link to={"/cart"} className='text-2xl relative'>
                           <span><FaShoppingCart/></span>
       
-                          <div className='bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3'>
+                          {/* <div className='bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3'>
                               <p className='text-sm'>{context?.cartProductCount}</p>
-                          </div>
+                          </div> */}
                       </Link>
                       )
                   }
               
 
-
+{/* 
                 <div>
                   {
                     user?._id  ? (
                       <button onClick={handleLogout} className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>Logout</button>
                     )
                     : (
-                    <Link to={"/login"} className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>Login</Link>
                     )
                   }
                     
-                </div>
+                </div> */}
+                    <Link to={"/login"} className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>Login</Link>
 
             </div>
 
